03056cbf98c44f34d184a352f5340b1eb03de5ae
Kenji Saito <ken-yo@mbr.nifty.com>
2022-04-28 14:45:04 +0900

    Fix the error

--- App.tsx
+++ App.tsx
@@ -71,7 +71,7 @@ const App = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
+        {/* <Header /> */}
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
